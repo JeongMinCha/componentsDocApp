b8fae78797e784ab9958c5cac1ef012e0660ca71
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-01-19 22:42:24 +0900

    action-sheet

---
+++ src/pages/action-sheet/action-sheet.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { ActionSheetController } from 'ionic-angular';
+
+@Component({
+    templateUrl: 'action-sheet.html'
+})
+export class ActionSheetPage {
+    constructor(public actionSheetCtrl: ActionSheetController) {
+
+    }
+
+    presentActionSheet() {
+        let actionSheet = this.actionSheetCtrl.create({
+            title: 'action sheet demo',
+            buttons: [
+                {
+                    text: 'Delete',
+                    role: 'destructive',
+                    handler: () => {
+
+                    }
+                },
+                {
+                    text: 'button',
+                    handler: () => {
+
+                    }
+                },
+                {
+                    text: 'cancel',
+                    role: 'cancel',
+                    handler: () => {
+
+                    }
+                }
+            ]
+        });
+        actionSheet.present();
+    }
+}
