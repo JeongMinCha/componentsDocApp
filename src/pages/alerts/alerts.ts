25e7a775c3600ee370e4bbef941bd041a106f08e
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-01-19 23:25:58 +0900

    alerts-lecture checkpoint 2

--- src/pages/alerts/alerts.ts
+++ src/pages/alerts/alerts.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 
 @Component({
     templateUrl: 'alerts.html',
 })
 export class AlertsPage {
-    constructor() {
+    constructor(public alertCtrl: AlertController) {
         
     }
 
     showBasicAlert() {
-
+        let alert = this.alertCtrl.create({
+            title: 'Basic Alert!',
+            subTitle: 'This is an ui component called a basic alert',
+            buttons: ['OK'],
+        });
+        alert.present();
     }
 
     showPromptAlert() {
-
+        
     }
 
     showConfirmationAlert() {
