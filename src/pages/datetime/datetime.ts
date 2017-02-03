40a6d7324e171132d3229bf0d730a29234659417
jeongmincha <jeongmin.cha@kaist.ac.kr>
2017-02-04 02:02:27 +0900

    datetime finished

--- src/pages/datetime/datetime.ts
+++ src/pages/datetime/datetime.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     templateUrl: 'datetime.html',
+    selector: 'page-datetime',
 })
 export class DateTimePage {
+    startDate = '2000-01-01';
     constructor() {
 
     }
