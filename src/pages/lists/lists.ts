a43363938919d640cdafeef520d4d4333627d38e
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-02-10 22:36:13 +0900

    Complete ListsPage

--- src/pages/lists/lists.ts
+++ src/pages/lists/lists.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Person } from '../../models/person';
 
 @Component({
     templateUrl: 'lists.html'
 })
 export class ListsPage {
-    constructor() {
+    persons: Person[] = [];
 
+    constructor() {
+        this.persons.push(new Person('assets/img/venkman.jpg','Venkman','Back off man, I\'m a scientist'));
+        this.persons.push(new Person('assets/img/spengler.jpg','Egon','We\'re gonna go full stream.'));
+        this.persons.push(new Person('assets/img/stantz.jpg','Ray','Ugly little spud, isn\'t he?'));
     }
 }
