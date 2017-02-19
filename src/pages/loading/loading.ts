fc2c3a63f44f4f01df20d48c23bf3dc074bda3e2
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-02-20 02:12:15 +0900

    Loading Page 2

--- src/pages/loading/loading.ts
+++ src/pages/loading/loading.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { LoadingController } from 'ionic-angular';
 
 @Component({
     templateUrl: 'loading.html'
 })
 export class LoadingPage {
-    constructor() {
+    constructor(public loadingCtrl: LoadingController) {
 
+    }
+
+    presentLoading() {
+        let loader = this.loadingCtrl.create({
+            spinner: 'bubbles',
+            content: 'Please wait...',
+            duration: 3000,
+        });
+        loader.present();
     }
 }
