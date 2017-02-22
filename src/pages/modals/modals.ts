658ccc20d21817a4dbd98022387314e634793878
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-02-23 01:59:36 +0900

    Modal Page

--- src/pages/modals/modals.ts
+++ src/pages/modals/modals.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+class Email {
+    address: string;
+    body: string;
+
+    constructor(addr, body) {
+        this.address = addr;
+        this.body = body;
+    }
+}
+
 @Component({
     templateUrl: 'modals.html',
 })
 export class ModalPage {
+    emails: Array<Email>;
+
     constructor() {
-        
+        this.emails = [
+            { address: "john@gmail.com", body: "I think it was a wonderful vacation. Do you think so?" },
+            { address: "jake@gmail.com", body: "Hi" },
+            { address: "sarah@gmail.com", body: "What\'s up?" },
+        ];
     }
 }
