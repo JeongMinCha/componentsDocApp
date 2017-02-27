7352bac4d093e3995bb5f0f87a0a6a268b8f4c23
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-02-27 23:07:38 +0900

    Range Page 2

--- src/pages/range/range.ts
+++ src/pages/range/range.ts
@@ -4,5 +4,7 @@ import { Component } from '@angular/core';
     templateUrl: 'range.html'
 })
 export class RangePage {
-
+    contrast: number = 0;
+    strokes = { lower: 30, upper: 60 };
+    warmth: number = 120;
 }
