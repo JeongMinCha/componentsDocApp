d2b795625a3fa098b705b74fcbea89c17f4bee68
Jeongmin Cha <jeongmin.cha@kaist.ac.kr>
2017-03-10 15:25:42 +0900

    Slide Page 3

--- src/pages/slides/slides.ts
+++ src/pages/slides/slides.ts
@@ -4,5 +4,21 @@ import { Component } from '@angular/core';
     templateUrl: 'slides.html'
 })
 export class SlidePage {
-
+    slides = [
+        {
+            img: "assets/img/ica-slidebox-img-1.png",
+            title: "Welcome to the Docs!",
+            content: "The <b>Ionic Component Documentation</b> showcases a number of useful components that are included out of the box with Ionic.",
+        },
+        {
+            img: "assets/img/ica-slidebox-img-2.png",
+            title: "What is Ionic?",
+            content: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
+        },
+        {
+            img: "assets/img/ica-slidebox-img-3.png",
+            title: "What is Ionic Cloud?",
+            content: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
+        }
+    ];
 }
